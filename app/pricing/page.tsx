4d1d467b23b4b89d5da53dc3c721657048d6639a
Puneet Kandpal <puneet.kandpal@gmail.com>
2026-03-02 20:58:42 +0530

    Remove platform and pricing pages

--- app/pricing/page.tsx
+++
@@ -1,9 +0,0 @@
-import { PricingCards } from "./components/PricingCards";
-
-export default function PricingPage() {
-  return (
-    <div className="flex flex-col w-full">
-      <PricingCards />
-    </div>
-  );
-}
